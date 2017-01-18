a47809a917d98b312de9b6c79436a441d8d479f6
Zachary <zbbergma@gmail.com>
2017-01-18 21:58:06 +0000

    (update) near end of function-master

---
+++ projects/function-master/function-master.js
@@ -0,0 +1,134 @@
+function objectValues(obj) {
+    var output = [];
+    for (let key in obj) {
+        output.push(obj[key]);
+    }
+    return output;
+}
+
+function keysToString(obj) {
+    var strOutput = '';
+    for(let key in obj) {
+        strOutput = strOutput + key + " ";
+    }
+    return strOutput.trim();
+}
+
+function valuesToString(obj) {
+    var arrayOfVals =[];
+    for(let key in obj) {
+        if(typeof obj[key] === 'string') {
+            arrayOfVals.push(obj[key]);
+        }
+    }
+    return arrayOfVals.join(' ');
+}
+
+function arrayOrObject(data) {
+    if(Array.isArray(data)){
+        return 'array';
+    } else if(typeof data === 'object' && data !== null && data instanceof Date !== true) {
+        return 'object';
+    }
+}
+
+function capitalizeWord(str) {
+    return str.toUpperCase().slice(0,1) + str.slice(1);
+}
+
+function capitalizeAllWords(str) {
+    var strArray = str.split(/\s+/gi);
+    for(let i = 0 ; i < strArray.length ; i++) {
+        strArray[i] = capitalizeWord(strArray[i]);
+    }
+    return strArray.join(" ");
+}    
+
+function welcomeMessage(obj) {
+    if(obj.name) {
+        return 'Welcome ' + capitalizeWord(obj.name) + '!';
+    } 
+}
+
+function profileInfo(obj) {
+    if(obj.name && obj.species) {
+        return capitalizeWord(obj.name) + ' is a ' + capitalizeWord(obj.species);
+    }
+}
+
+function maybeNoises(obj) {
+    if(Array.isArray(obj.noises) && obj.noises.length) {
+        return obj.noises.join(" ");
+    }
+    return 'there are no noises';
+}
+
+function hasWord(string, word) {
+    var regex = new RegExp(word, 'gi');
+    if (string.match(regex) !== null && string.match(regex).length ) {
+        return true;
+    }
+    return false;
+}
+
+function addFriend(name, obj) {
+    obj.friends.push(name);
+    return obj;
+}
+
+function isFriend(name, obj) {
+    for(let i = 0 ; obj.friends && i < obj.friends.length ; i++) {
+        if(obj.friends[i] === name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function nonFriends(name, listPeople) {
+    let outputNonFriends = [];
+    var personFriends;  //will friends of name
+    for(let i = 0 ; i < listPeople.length ; i++) {
+        if(listPeople[i].name === name){
+            personFriends = listPeople[i].friends;
+        }
+    }
+    
+    //now to find names of nonfriends and add them to outputNonFriends
+    //case where name has no friends
+    if(personFriends.length === 0) {
+        for(let i = 0 ; i < listPeople.length ; i++) {
+            if(listPeople[i].name !== name) outputNonFriends.push(listPeople[i].name);
+        }
+        return outputNonFriends;
+    }
+    
+    //now the case where personFriends has values
+    for (let i = 0 ; i < personFriends.length ; i++) {
+        for(let j = 0 ; j < listPeople.length ; j++) {
+            if(listPeople[j].name === personFriends[i]){
+                break;
+            }
+            if(j === listPeople.length - 1 && listPeople[j].name !== personFriends[i]) {
+                outputNonFriends.push(personFriends[i]);
+            }
+        }
+    }    
+    return outputNonFriends;
+} 
+
+function updateObject(obj, key, value) {
+    obj[key] = value;
+}
+
+function removeProperties(obj , strArray) {
+    for(let i = 0 ; i < strArray.length ; i++) {
+        delete obj[strArray[i]];
+    }
+}
+
+function dedup() {
+    
+}
+
+
