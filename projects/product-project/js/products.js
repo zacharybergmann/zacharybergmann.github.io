671093a2b3208ad81c44b0d59964177392bb21e4
Zachary <zbbergma@gmail.com>
2017-02-07 23:03:55 +0000

    (update)continue on product project, stable

--- projects/product-project/js/products.js
+++ projects/product-project/js/products.js
@@ -4,44 +4,55 @@ $(function () {
   $.getJSON('./data/product.json', function(data){
     //start working here
     
-    
     /********** Add all items to the page ******************/
     const prodArray = data.map(function(product, index, products){
-      //create a layout for each product to have on the page
       
-      var $div = $('<div>')
-        .attr("class", "item-outer-div")
-        .attr('class', 'product');
+      //create outer container div tag for whole product
+      var $productWrapperDiv = $('<div>')
+        .attr('class', 'row')
+        .attr('id', 'product-wrapper-div')
+        .css('margin-right', '40px');
         
+      //create a div to hold the image
+      var $imgDiv = $('<div>')
+        .attr('id', 'img-div')
+        .attr('class', 'col-md-2')
+        .css('text-align', 'center');
+        
+      //get image thumbnail created   
       var imgPath = "./img/product/thumbs/" + product.image;
       
       var $thumbnailImg = $("<img>")
         .attr("src", imgPath)
-        .attr('class', 'product');
-      
-      var $descDiv = $('<div>')
-      .attr('class', 'product');
-        
-      var $h4 = $("<h4>")
-        .text(product.desc)
-        .attr('class', 'product');
-        
-      var $price = $('<h5>')
-      .attr('class', 'product')
-        .text(product.price.toLocaleString('en-US', {style: "currency", currency: 'USD', maximumFractionDigits: 2, minimumFractionDigits: 2 }));
-        
-      var $availColors = $('<select>')
         .attr('class', 'product')
-        .css('type', 'radio');
+        .css('max-width', '160px')
+        .css('max-height', '160px')
+        .css('width', 'auto')
+        .css('height', 'auto');
       
-      $availColors.append($("<option>").attr("value", 'no-color-selected').attr("selected", "selected").text('-- Select a Color --'));
+      $imgDiv.append($thumbnailImg);
       
-      if(product.availableColors.length) {
-        product.availableColors.forEach(function(str, index, strs){
-          $availColors.append($("<option>").attr("value", str).text(str));
-        });
-      } 
+      //make a div to hold the description and specs of the product
+      var $infoDiv = $('<div>')
+        .attr('id', 'prod-info-div')
+        .attr('class', 'col-md-8');
+        
+      //make a div for the product name and a div for the product specs
+      var $descriptionDiv = $('<div>')
+        .attr('id', 'description-div');
+        
+      var $specsDiv = $('<div>')
+        .attr('id', 'specs-div'); 
+        
+      //create the name of product and the specs   
+      var $h4 = $("<h4>")
+        .text(product.desc);
+        
+      $h4.appendTo($descriptionDiv);
       
+      $infoDiv.append($descriptionDiv);
+      
+        
       var $specsP = "";
       if(product.specs.length) {
         product.specs.forEach(function(str, index, strs){
@@ -51,16 +62,68 @@ $(function () {
         $specsP += "<p>" + "No Specifications Available" + "</p>";
       }
       
-      $descDiv.append($h4);
-      $descDiv.append($specsP);
-      $div.append($thumbnailImg);
-      $div.append($descDiv);
-      $div.append($availColors);
-      $div.append($price);
-      $div.appendTo('main');
+      $specsDiv.append($specsP);
+      $infoDiv.append($specsDiv);
       
-      return $div;
       
+      //create a container to hold the price, color dropdown
+      var $priceColorCartDiv = $('<div>')
+        .attr('id', 'price-color-cart-div')
+        .attr('class', 'col-md-2');
+        
+      var $priceDiv = $('<div>')
+        .attr('id', 'price-div');
+        
+      var $priceHead = $('<h5>')
+        .attr('class', 'product')
+        .css('text-align', 'right')
+        .text(product.price.toLocaleString('en-US', {style: "currency", currency: 'USD', maximumFractionDigits: 2, minimumFractionDigits: 2 }));
+        
+      var $cartDiv = $('<div>')
+        .attr('id', 'cart-div');
+        
+      var $colorDDDiv = $('<div>')
+        .attr('id', 'color-dropdown-div')
+        .css('text-align', 'right');
+      
+      var $availColors = $('<select>')
+        .attr('class', 'form')
+        .attr('id', 'color-dropdown')
+        .css('type', 'radio');
+      
+      $availColors.append($("<option>")
+        .attr("value", 'no-color-selected')
+        .attr("selected", "selected")
+        .text('-- Select a Color --')
+        .css('float', 'right'));
+      
+      if(product.availableColors.length) {
+        product.availableColors.forEach(function(str, index, strs){
+          $availColors.append($("<option>").attr("value", str).text(str));
+        });
+      } 
+      
+      var $inStockDiv = $('<div>')
+      
+      
+      $priceDiv.append($priceHead);
+      $colorDDDiv.append($availColors);
+      
+      $priceColorCartDiv.append($('<br>'));
+      $priceColorCartDiv.append($colorDDDiv);
+      $priceColorCartDiv.append($('<br>'));
+      $priceColorCartDiv.append($priceDiv);
+      $priceColorCartDiv.append($cartDiv);
+      
+      $productWrapperDiv.append($imgDiv);
+      $productWrapperDiv.append($infoDiv);
+      $productWrapperDiv.append($priceColorCartDiv);
+      
+      $('main').append($productWrapperDiv);
+      $('main').append($('<hr>'));
+      $('main').append($('<br>'));
+      
+      return $productWrapperDiv;
     });
     
     /********************** Make some event listeners ********************/
@@ -70,7 +133,17 @@ $(function () {
       if($('search-bar-input:focus') && (event.keyCode === 13)){
         //search thru the array prodArray
         //detatch each item that does not meet the description
-        
+        var searchFor = $(this).val();
+        //search for products via selector
+        //search thru all of them for the desired word in various properties
+        //remove those that do not meet the description
+        $('main').detach();
+        prodArray.map(function(product, index, products){
+          if(product) {
+              //do something
+            
+          } 
+        });
       }
     });
     
@@ -102,7 +175,7 @@ $(function () {
       
       var $span = $('<span>')
         .attr('class', 'close')
-        .text('&times;');
+        .html('&times;');
         
       var $img = $('<img>')
         .attr('class', 'modal-content')
@@ -118,17 +191,23 @@ $(function () {
       
       $('.close').on('click', function(event){
         $('.modal').css('display', 'none');
+        $('.modal').remove();
       });
       
       $('.modal').on('click', function(event){
-        $('.modal').css('display', 'none');  
+        $('.modal').css('display', 'none');
+        $('.modal').remove();
       });
       
-      console.log(newTarget);
+      //console.log(newTarget);
       
       
       //TODO: need to get the x box to close the modal
       //TODO: maybe downsize the image for the modal
+      //TODO: need to remove the modal after click outside of the box or click x box
+      //Gradient header with CSS
+      //name for company
+      //many more events
       
     });
     
